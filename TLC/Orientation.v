5da2aa7e9e7f33c4e578897deb556f3cb0709f41
Jeremiah Griffin <jgrif007@ucr.edu>
2018-12-20 16:01:28 -0800

    Add equality for orientation

--- TLC/Orientation.v
+++ TLC/Orientation.v
@@ -6,3 +6,16 @@ Inductive orientation : Type :=
 | Request
 | Indication
 | Periodic.
+
+Definition orientation_eqb (x y : orientation) : bool :=
+  match x, y with
+  | Request, Request => true
+  | Indication, Indication => true
+  | Periodic, Periodic => true
+  | _, _ => false
+  end.
+
+Lemma orientation_eq_dec (x y : orientation) : {x = y} + {x <> y}.
+Proof.
+  decide equality.
+Qed.
