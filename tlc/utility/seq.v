106d6ef3d202c889b8e77c6cb958e1ee91465553
Jeremiah Griffin <jgrif007@ucr.edu>
2019-03-01 15:54:32 -0800

    Add computational extension predicate

--- tlc/utility/seq.v
+++ tlc/utility/seq.v
@@ -1,4 +1,6 @@
+Require Import mathcomp.ssreflect.eqtype.
 Require Import mathcomp.ssreflect.seq.
+Require Import mathcomp.ssreflect.ssrbool.
 Require Import mathcomp.ssreflect.ssreflect.
 Require Import mathcomp.ssreflect.ssrfun.
 Require Import tlc.utility.applicative.
@@ -8,6 +10,24 @@ Require Import tlc.utility.monad.
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
+
+(* Predicate: xs' is an extension of xs <-> xs is a suffix of xs' *)
+Fixpoint extension (T : eqType) (xs' xs : seq T) :=
+  if xs' == xs then true else
+  if xs' is x :: xs' then extension xs' xs else false.
+
+Lemma extension_nil_suffix (T : eqType) (xs' : seq T) :
+  extension xs' [::].
+Proof.
+  by elim: xs'.
+Qed.
+
+Lemma extension_cons (T : eqType) (xs' xs : seq T) x :
+  extension xs' xs ->
+  extension (x :: xs') xs.
+Proof.
+  by move=> H; rewrite /=; case: (x :: xs' == xs).
+Qed.
 
 (* Maps over the fsts in a sequence of pairs *)
 Fixpoint map1 a b a' (f : a -> a') (ps : seq (a * b)) :=
