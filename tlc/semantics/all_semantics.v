4e7c28c4e777ae5a162cc2bcbe238012603311da
Jeremiah Griffin <nokurn@gmail.com>
2019-03-10 21:17:17 -0700

    Add lower_assertion function

--- tlc/semantics/all_semantics.v
+++ tlc/semantics/all_semantics.v
@@ -2,6 +2,7 @@ Require Export tlc.semantics.assertion.
 Require Export tlc.semantics.environment.
 Require Export tlc.semantics.equivalents.
 Require Export tlc.semantics.error.
+Require Export tlc.semantics.lower.
 Require Export tlc.semantics.pattern.
 Require Export tlc.semantics.predicate.
 Require Export tlc.semantics.push.
---
+++ tlc/semantics/lower.v
@@ -0,0 +1,14 @@
+Require Import mathcomp.ssreflect.ssreflect.
+Require Import tlc.semantics.push.
+Require Import tlc.semantics.restrict.
+Require Import tlc.syntax.all_syntax.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+(* Implements component lowering by first pushing an assertion then
+ * restricting it *)
+Definition lower_assertion (i : nat) A (TI : top_invariant A) :=
+  restrict_assertion {A: "Fd" <<< [i]}
+    (projT1 (push_assertion i (location_invariant_assertion TI))).
