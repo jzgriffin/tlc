16fb49798bcb546d63ea396c8e6bd4c0401dcfc1
Jeremiah Griffin <jgrif007@ucr.edu>
2021-01-24 21:03:01 -0800

    Rewrite L42 proof in new system

--- tlc/component/perfect_link_2.v
+++ tlc/component/perfect_link_2.v
@@ -57,7 +57,226 @@ Lemma L42 Delta : Context Delta [::] ||- perfect_link, {-A
   )
 -}.
 Proof.
-Admitted.
+  dforall n; dforall n'; dforall m; dif.
+
+  (* By InvLSe *)
+  dhave {-A
+    self (
+      on n, CPLDeliver ' n' ' m \in Fois =>>
+      exists: (* c *)
+      (n', $$0) \in (Fs' ' n).2 /\
+      on n, event[0]<- CSLDeliver ' n' ' ($$0, m)
+    )
+  -}.
+  {
+    eapply DSCut; first (repeat dclear; by apply DPInvLSe with
+      (A := {-A
+        on n, CPLDeliver ' n' ' m \in Fois ->
+        exists: (* c *)
+        (n', $$0) \in (Fs' ' n).2 /\
+        on n, event[0]<- CSLDeliver ' n' ' ($$0, m)
+      -});
+      [by dautoclosed | by repeat constructor]).
+
+    (* request *)
+    difp.
+    {
+      dforall e; dif; dsplitp; dswap; dsimplp.
+      dcase {-t ? e -}; dexistsp e_m; dexistsp e_n'; dtgenp;
+        dtsubstep_l; dsimplp; dxchg 0 2; dswap;
+        dtsubstep_l; dswap; dclear; dswap.
+      dcase {-t ? Fs ' ? Fn -}; dexistsp s_r; dexistsp s_c; dtgenp;
+        dtsubstep_l; dsimplp; dswap; dclear.
+      dtinjectionp; repeat dsplitp.
+      do 2 dclear; dtgenp; dtsubste_l; dclear.
+      by dif; dsplitp; dclear; dpmembernilp.
+    }
+
+    (* indication *)
+    difp.
+    {
+      dforall i; dforall e; dif; dsplitp; dswap; dsimplp.
+      dcase {-t (? i, ? e) -}; dexistsp e_m; dexistsp e_c; dexistsp e_n';
+        dtinjectionp.
+      dsplitp; dtgenp;
+        dxchg 1 2; dtsubstep_l;
+        dswap; dxchg 1 3; dtsubstep_l;
+        dswap; dclear; dautoeq.
+      dswap; dtgenp;
+        dtsubstep_l;
+        dswap; dxchg 1 2; dtsubstep_l;
+        dswap; dclear; dautoeq.
+      dcase {-t ? Fs ' ? Fn -}; dexistsp s_r; dexistsp s_c; dtgenp;
+        dtsubstep_l; dsimplp; dswap; dclear.
+      dcase {-t FCount ' (? e_n', ? e_c) ' ? s_r == 0 -}; dorp; dtgenp;
+        dtsubstep_l; dsimplp; dswap; dclear;
+        dtinjectionp; repeat dsplitp; dswap; dclear; dswap; dtgenp;
+        dtsubste_l; dclear;
+        dif; dsplitp; last by dswap; dpmembernilp.
+      dexists e_c.
+      dtgenp; dxchg 1 2; dtsubste_l; dtsubstep_l; dswap; dclear.
+      dtgenp; dtsubste_l; dclear.
+      dpmembersingletonp; dtinjectionp; dsplitp.
+      do 2 (dtgenp; dtsubste_l; dautoeq; dclear).
+      dsplit; [| by dsplit; first duse DPEqual].
+      dsimpl; dpmembersetunion; dright.
+      by duse DPMemberSingleton; dforallp {-t (e_n', e_c) -}.
+    }
+
+    (* periodic *)
+    difp.
+    {
+      dif; dsplitp; dswap; dsimplp.
+      dtinjectionp; repeat dsplitp;  do 2 dclear; dtgenp; dtsubste_l.
+      by dif; dsplitp; dclear; dpmembernilp.
+    }
+
+    by [].
+  }
+
+  (* By lemma 90 *)
+  eapply DSCut; first (by repeat dclear; apply DTL87 with
+    (A := {-A on n, event[0]<- CSLDeliver ' n' ' ($$0, m) -}));
+    dtsubstposp.
+
+  (* By lemma 43 *)
+  dhave {-A
+    forall: (* c *)
+    self (
+      ((n', $$0) \in (Fs ' n).2 /\
+        eventuallyp on n, event[0]<- CSLDeliver ' n' ' ($$0, m)) =>>
+        Fn = n -> CPLDeliver ' n' ' m \notin Fois
+    )
+  -}; first by
+    dforall c; duse L43; dforallp n; dforallp n'; dforallp m;
+    dforallp c; difp; first by dassumption.
+
+  (* By rule InvSSe *)
+  dhave {-A
+    forall: (* c *)
+    self (
+      (n', $$0) \in (Fs ' n).2 =>>
+      always ((n', $$0) \in (Fs ' n).2)
+    )
+  -}.
+  {
+    dforall c.
+    eapply DSCut; first (by repeat dclear; apply DPInvSSe with
+      (S0 := {-A forall: (* s *) (n', c) \in ($$0).2 -});
+      [by [] | by dautoclosed | by repeat constructor]);
+      dforallp n.
+    (* request *)
+    difp.
+    {
+      dforall s; dforall e; dsimpl; dif.
+      dcase {-t ? e -}; dexistsp e_m; dexistsp e_n; dtgenp;
+        dtsubste_l; dautoeq; dclear.
+      by dcase {-t ? s -}; dexistsp s_r; dexistsp s_c; dtgenp;
+        dtsubste_l; dautoeq; dtsubstep_l; dswap; dclear; dsimpl; dsimplp.
+    }
+    (* indication *)
+    difp.
+    {
+      dforall s; dforall i; dforall e; dsimpl; dif.
+      dcase {-t (? i, ? e) -}; dexistsp e_m; dexistsp e_c; dexistsp e_n'; dtgenp;
+        dtsubste_l; dautoeq; dclear.
+      dcase {-t ? s -}; dexistsp s_r; dexistsp s_c; dtgenp;
+        dtsubste_l; dautoeq; dtsubstep_l; dswap; dclear; dsimpl; dsimplp.
+      dcase {-t FCount ' (? e_n', ? e_c) ' ? s_r == 0 -}; dorp; dtgenp;
+        dtsubste_l; dsimpl; dclear; last by [].
+      by dpmembersetunion; dleft.
+    }
+    (* periodic *)
+    difp.
+    {
+      by dforall s; dsimpl; dif.
+    }
+    by dsimplp.
+  }
+
+  (* By lemma 107 *)
+  dhave {-A
+    forall: (* c *)
+    self (
+      eventuallyp (on n, event[0]<- CSLDeliver ' n' ' ($$0, m)) =>>
+      always eventuallyp (on n, event[0]<- CSLDeliver ' n' ' ($$0, m))
+    )
+  -}.
+  {
+    dforall c.
+    eapply DSCut; first (by repeat dclear; apply DTL104 with
+      (A := {-A on n, event[0]<- CSLDeliver ' n' ' (c, m) -})).
+    by eapply DSCut; first (by eapply DTL129); difp; first by dhead.
+  }
+
+  (* From (3) and (4) *)
+  dhave {-A
+    forall: (* c *)
+    self (
+      (n', $$0) \in (Fs ' n).2 /\
+        eventuallyp (on n, event[0]<- CSLDeliver ' n' ' ($$0, m)) =>>
+      always ((n', $$0) \in (Fs ' n).2) /\
+        always eventuallyp (on n, event[0]<- CSLDeliver ' n' ' ($$0, m))
+    )
+  -}.
+  {
+    dforall c.
+    dforallp c; dswap; dforallp c.
+    match goal with
+    | |- Context _ ({-A self ?A_ -} :: {-A self ?B_ -} :: _) ||- _, _ =>
+      eapply DSCut; first (by repeat dclear; apply DTSelfDistribAnd with
+        (A := A_) (B := B_))
+    end; dsplitp; dclear; difp; first by dsplit; dassumption.
+    by dtunionentails; dtsubstposp.
+  }
+  do 2 (dswap; dclear).
+
+  (* By lemma 100 on (5) and (2) *)
+  dhave {-A
+    forall: (* c *)
+    self (
+      (n', $$0) \in (Fs ' n).2 /\
+        eventuallyp (on n, event[0]<- CSLDeliver ' n' ' ($$0, m)) =>>
+      always (Fn = n -> CPLDeliver ' n' ' m \notin Fois)
+    )
+  -}.
+  {
+    dforall c.
+    do 2 (dforallp c; dswap).
+    match goal with
+    | |- context[ {-A always ?A1_ /\ always ?A2_ -} ] =>
+      eapply DSCut; first (by repeat dclear; apply DTL128_1 with
+        (A := A1_) (B := A2_))
+    end; dtsubstp_r.
+    match goal with
+    | |- Context _ ({-A self ?A_ -} :: {-A self ?B_ -} :: _) ||- _, _ =>
+      eapply DSCut; first (by repeat dclear; apply DTSelfDistribAnd with
+        (A := A_) (B := B_))
+    end; dsplitp; dclear; difp; first by dsplit; dassumption.
+    by dtl97; dtsubstposp.
+  }
+  do 2 (dswap; dclear).
+
+  (* By ASASe on (1) and (6) *)
+  eapply DSCut; first (by repeat dclear; apply DPASASe' with
+    (S0 := {-A forall: (* s *) (n', $$1) \in ($$0).2 /\
+      eventuallyp on n, event[0]<- CSLDeliver ' n' ' ($$1, m) -})
+    (A := {-A on n, CPLDeliver ' n' ' m \in Fois -})
+    (A' := {-A Fn = n -> CPLDeliver ' n' ' m \notin Fois -});
+    [by [] | by dautoclosed | by dautoclosed | by dautoclosed]);
+    dforallp n; dsimplp; repeat (difp; first by dassumption).
+  do 2 (dswap; dclear).
+
+  (* By lemma 108 on (8) *)
+  rewrite /AIf /ANotIn; dsdemorgan_1r; dtgenp.
+  dtsubstp_r_keep; dswap; dtsubst_l; dclear; dswap; dclear.
+  eapply DSCut; first by dhead.
+  dtl105_1; dtsubstposp.
+  dswap; eapply DSCut; [eapply DTSelfDistribAnd |]; dsplitp; dclear; difp;
+    first by dsplit; [by dhead | by dswap; dhead].
+  do 2 (dswap; dclear).
+  by dtunionentails; dtsubstposp; dtandelimself; dtsubstp_l.
+Qed.
 
 (* No duplication
  * If a message is sent at most once, it will be delivered at most once.
